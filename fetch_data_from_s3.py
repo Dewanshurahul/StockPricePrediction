10b82fd7d49c51c2583e22abb9bf62fe8af035da
Dewanshu Rahul4 <dewanshurahul95@gmail.com>
2020-07-01 05:52:39 +0530

    Refactor: Removing test code and uncommenting right code.

--- fetch_data_from_s3.py
+++ fetch_data_from_s3.py
@@ -1,26 +1,20 @@
-# import boto3
-# import pandas as pd
-# from pyspark import SparkContext
-# from pyspark.sql import SQLContext
-# client = boto3.client("s3")
-# path = "s3://stockpriceprediction/HistoricalQuotes.csv"
-# df = pd.read_csv(path)
-# # print(df.head(20))
-# sc = SparkContext()
-# sqlContext = SQLContext(sc)
-# sparkdf = sqlContext.createDataFrame(df)
-# # print(sparkdf.head(5))
-# x = sparkdf.head(5)
-# print(x)
-# sc.stop()
-
 import boto3
+import pandas as pd
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
-client = boto3.client('s3')
-sc = SparkContext()
-sqlContext = SQLContext(sc)
-path="s3://stockpriceprediction/HistoricalQuotes.csv"
+try:
+    client = boto3.client("s3")
+    path = "s3://stockpriceprediction/HistoricalQuotes.csv"
+    df = pd.read_csv(path)
+    # print(df.head(20))
+except:
+    print("Please Check path for given file")
 
-df = sqlContext.read.format('com.databricks.spark.csv').options(header='true').load(path)
-print(df.head(20))
+try:
+    sc = SparkContext()
+    sqlContext = SQLContext(sc)
+    sparkdf = sqlContext.createDataFrame(df)
+    print(sparkdf.head(5))
+except:
+    print("Check for pandas DataFrame")
+sc.stop()
